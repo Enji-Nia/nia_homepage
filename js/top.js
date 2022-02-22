7c1fa724e899b3deb470204da44e8a11c2dfa580
Enji-Nia <pj.sai.nia.enji@gmail.com>
2022-02-23 00:29:01 +0900

    first commit

---
+++ js/top.js
@@ -0,0 +1,9 @@
+$('.top-preview').slick({
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	speed: 500,//スライドのスピード。初期値は300。
+	slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+	centerMode: true,//要素を中央ぞろえにする
+	variableWidth: true,//幅の違う画像の高さを揃えて表示
+	dots: true,//下部ドットナビゲーションの表示
+});
